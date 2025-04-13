68dfb631f33081c579c5dc2be00c811f44c696ea
keithkipkoech <keithselim@gmail.com>
2025-04-14 02:16:19 +0300

    new

---
+++ .history/src/components/Task_20250414021503.js
@@ -0,0 +1,16 @@
+// src/components/Task.js
+import React from "react";
+
+function Task({ text, category, onDelete }) {
+  return (
+    <div className="task">
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={onDelete}>
+        X
+      </button>
+    </div>
+  );
+}
+
+export default Task;
